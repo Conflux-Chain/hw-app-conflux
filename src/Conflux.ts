805fcc9caf9cb9a411ca54555f91ef39978587e2
chao.zhou <zctocm@gmail.com>
2021-12-11 16:26:38 +0800

    feat: api-getAppConfiguration

--- src/Conflux.ts
+++ src/Conflux.ts
@@ -156,6 +156,32 @@ export default class Conflux {
     );
   }
 
+  async getAppConfiguration(): Promise<{
+    name: string;
+    version: string;
+    flags: number | Buffer;
+  }> {
+    const r = await this.transport.send(0xb0, 0x01, 0x00, 0x00);
+    let i = 0;
+    const format = r[i++];
+
+    if (format !== 1) {
+      throw new Error("getAppAndVersion: format not supported");
+    }
+
+    const nameLength = r[i++];
+    const name = r.slice(i, (i += nameLength)).toString("ascii");
+    const versionLength = r[i++];
+    const version = r.slice(i, (i += versionLength)).toString("ascii");
+    const flagLength = r[i++];
+    const flags = r.slice(i, (i += flagLength));
+    return {
+      name,
+      version,
+      flags,
+    };
+  }
+
   private pathToBuffer(originalPath: string) {
     const path = originalPath
       .split("/")
